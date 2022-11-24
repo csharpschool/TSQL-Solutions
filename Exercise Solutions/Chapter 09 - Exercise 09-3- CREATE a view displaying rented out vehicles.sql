4111585137ae91408805386ef186de975891b98a
Jonas Fagerberg <info@csharpschool.com>
2022-11-24 14:40:36 +0100

    Changed name on files

--- Exercise Solutions/Chapter 09 - Exercise 09-3- CREATE a view displaying rented out vehicles.sql
+++ Exercise Solutions/Chapter 09 - Exercise 09-3- CREATE a view displaying rented out vehicles.sql
@@ -1,9 +1,11 @@
 ------------------------------------------------
 -- The SELECT statement for CREATING the view --
 ------------------------------------------------
-SELECT    dbo.Vehicle.VehicleID, dbo.Vehicle.RegistrationNumber, dbo.Vehicle.BasePricePerKm, dbo.Vehicle.BasePricePerDay, dbo.Vehicle.DayTariff, dbo.Vehicle.KmTariff, dbo.Vehicle.Meter, dbo.Vehicle.VehicleTypeID, dbo.VehicleType.Name AS VehicleType, dbo.Booking.Rented, 
-              dbo.Booking.Returned, dbo.Booking.Cost
-FROM      dbo.VehicleType INNER JOIN
-              dbo.Vehicle ON dbo.VehicleType.VehicleTypeID = dbo.Vehicle.VehicleTypeID LEFT OUTER JOIN
-              dbo.Booking ON dbo.Vehicle.VehicleID = dbo.Booking.VehicleID
-WHERE    (dbo.Booking.Rented IS NOT NULL) AND (dbo.Booking.Returned IS NULL) AND (dbo.Booking.Cost IS NULL)
+SELECT DISTINCT dbo.Vehicle.VehicleID, dbo.Vehicle.RegistrationNumber, dbo.Vehicle.BasePricePerKm, dbo.Vehicle.BasePricePerDay, dbo.Vehicle.DayTariff, dbo.Vehicle.KmTariff, dbo.Vehicle.Meter, dbo.Vehicle.VehicleTypeID, dbo.VehicleType.Name AS VehicleType
+FROM      dbo.Booking RIGHT OUTER JOIN
+              dbo.Vehicle ON dbo.Booking.VehicleID = dbo.Vehicle.VehicleID LEFT OUTER JOIN
+              dbo.VehicleType ON dbo.Vehicle.VehicleTypeID = dbo.VehicleType.VehicleTypeID
+WHERE    (dbo.Vehicle.VehicleID NOT IN
+                  (SELECT    VehicleID
+                  FROM      dbo.vwIsRentedOut))
+
